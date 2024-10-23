221a561f84999c81c9f4218c495fd6dc8fc8b36c
Phan Thanh Toan <phanthanhtoan248@gmail.com>
2024-10-23 22:35:58 +0700

    convert from  Celsius to Fahrenheit and reverse

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from "react";
 import "./styles.css";
 import ForecastDisplay from "./ForecastDisplay";
 
-function WeatherDisplay({city}) {
+function WeatherDisplay({ city }) {
   const [weather, setWeather] = useState(null);
+  const [isCelsius, setIsCelsius] = useState(true);
   useEffect(() => {
     const featchWeather = async () => {
       const API_Key = "01806323009c06702ba3db6c70d586c5";
@@ -26,49 +27,66 @@ function WeatherDisplay({city}) {
   }
 
   if (!weather.weather || weather.weather.length === 0) {
-    return (
-      <p className="text-center">
-          Please enter your city
-      </p>
-    );
+    return <p className="text-center">Please enter your city</p>;
   }
 
-  function getCurrentDate(){
+  function getCurrentDate() {
     const currentdate = new Date().toLocaleDateString();
     return currentdate;
+  }
+
+  function convertCelsius() {
+    return Math.floor(weather.main.temp - 273.15);
+  }
+
+  function convertFahrenheit() {
+    return Math.floor((convertCelsius() * (9 / 5) )+ 32);
+  }
+
+  function handleChangeCelsius() {
+    setIsCelsius((prevValue) => !prevValue);
+  }
+
+  function renderTemperature() {
+    if (isCelsius) {
+      return convertCelsius();
+    } else {
+      return convertFahrenheit();
+    }
   }
 
   return (
     <div className="flex justify-center items-center min-h-screen ">
       <div className="p-4 w-3/6 h-72 bg-gray-800 text-white rounded-lg shadow-xl">
-          <div className="flex justify-between">
-            <div className="flex flex-col justify-between">
-              <div className="mb-4">
-                <h1 className="text-2xl font-semibold">{weather.name}</h1>
-                <p className="text-sm capitalize">
-                  {weather.weather[0].description}
-                </p>
-                <p>{getCurrentDate()}</p>
-              </div>
-              <div className="mt-6 top-10">
-                <p className="text-6xl font-bold ">
-                  {Math.floor(weather.main.temp - 273.15)}°C
-                </p>
-              </div>
+        <div className="flex justify-between">
+          <div className="flex flex-col justify-between">
+            <div className="mb-4">
+              <h1 className="text-2xl font-semibold">{weather.name}</h1>
+              <p className="text-sm capitalize">
+                {weather.weather[0].description}
+              </p>
+              <p>{getCurrentDate()}</p>
             </div>
-            <div className="flex flex-col justify-between items-end">
-              <div className="w-16 h-16 bg-gray-600 rounded-full mb-4">
-                <img src="../images/sun.jpg" className="rounded-full"/>
-              </div>
-              <div className="text-sm space-y-2 text-right">
-                <p>Humidity: {weather.main.humidity}%</p>
-                <p>Wind: {Math.round(weather.wind.speed)} m/s</p>
-              </div>
+            <div className="mt-6 top-10">
+              <p className="text-6xl font-bold">{renderTemperature()}</p>
+              <p className="text-3xl font-bold " onClick={handleChangeCelsius}>
+                {isCelsius ? "°C" : "°F"} || {isCelsius ? "°F" : "°C"}
+              </p>
             </div>
           </div>
+          <div className="flex flex-col justify-between items-end">
+            <div className="w-16 h-16 bg-gray-600 rounded-full mb-4">
+              <img src="../images/sun.jpg" className="rounded-fulld" />
+            </div>
+            <div className="text-sm space-y-2 text-right">
+              <p>Humidity: {weather.main.humidity}%</p>
+              <p>Wind: {Math.round(weather.wind.speed)} m/s</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div>
-        <ForecastDisplay/ >
+        <ForecastDisplay />
       </div>
     </div>
   );
